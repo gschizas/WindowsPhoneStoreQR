7aa52b60d34c253810081a48ab75ecaf0307f99c
George Schizas <gschizas@gmail.com>
2015-08-13 17:21:38 +0300

    Adds support for new (Windows 10 era) app store

--- Chrome/content_script.js
+++ Chrome/content_script.js
@@ -2,17 +2,30 @@
 var pathParts = window.location.pathname.split('/');
 var appId = pathParts[pathParts.length - 1];
 
+var url = "";
+
 // Create placeholder
 
 var placeholder = $('<div/>', {id: 'qrCode', height: 200, width: 200});
-$('#offerRating').after(placeholder);
+if (window.location.host == "www.windowsphone.com") {
+    $('#offerRating').after(placeholder);
+    url = "http://windowsphone.com/s?appId=" + appId;
+}
+if (window.location.host == "www.microsoft.com") {
+    $('.ph-brand').after(placeholder);
+    url = "https://www.microsoft.com//store/apps//" + appId;
+    // double slash after microsoft.com to use a default language.
+    // double slash after apps to use the default name of the app.
+}
 
 // Insert QR Code
-var qrcode = new QRCode("qrCode", {
-    text: "http://windowsphone.com/s?appId=" + appId,
-    width: 200,
-    height: 200,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
-});
+if (url != "") {
+    var qrCode = new QRCode("qrCode", {
+        text: url,
+        width: 200,
+        height: 200,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.Q
+    });
+}
